d57ea36c43b7f9520eb0e2e338083f97dee4f7da
Vatcharapong <pexslipper@gmail.com>
2026-05-05 18:30:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -179,7 +179,7 @@ setInterval(() => {
   });
 
 
-  
-server.listen(3000, () => {
+const port = process.env.PORT || 4000 
+server.listen(port , () => {
   console.log('server running at http://localhost:3000');
 });
